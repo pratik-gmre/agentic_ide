efa19c6534aaf8ff98e45dca02f81d10c164702d
Pratik Ghimire <147188683+pratik-gmre@users.noreply.github.com>
2026-02-04 05:25:33 +0000

    firecrawl-setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,29 @@
+import { firecrawl } from "@/lib/firecrawl";
 import { inngest } from "./client";
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const { prompt } = event.data as { prompt: string };
+    const urls = (await step.run("extract-url", async () => {
+      return prompt.match(URL_REGEX) ?? [];
+    })) as string[];
+    const scrapedContent = await step.run("scrape-urls", async () => {
+      const results = await Promise.all(
+        urls.map(async (url) => {
+          const result = await firecrawl.scrape(url, { formats: ["markdown"] });
+          return result.markdown ?? null;
+        }),
+
+      );
+      return results.filter(Boolean).join("\n\n")
+    });
+    const finalPrompt = scrapedContent ? `Context:\n${scrapedContent}\n\nQuestion:${prompt}` : prompt
+    //  await step.run("generate-text",async()=>{
+    //     return await generateText
+    //  })
   },
 );
